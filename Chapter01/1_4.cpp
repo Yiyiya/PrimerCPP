087daa397235ebe462c376a63b160b9b9b7fd308
Yiyiya <jingfu1991@gmail.com>
2018-08-24 12:47:52 +0800

    1_2

--- Chapter01/1_4.cpp
+++ Chapter01/1_4.cpp
@@ -7,7 +7,7 @@ int main()
 //    cout << "Enter two number:" << endl;
 //    int v1=0, v2=0;
 //    cin >> v1 >> v2;
-//    cout << "The number of " << v1 << " and " << v2
+//    cout << "The sum of " << v1 << " and " << v2
 //         << " is "<< v1+v2<<endl;
 
 //    cout << " Hello World !" << endl;
@@ -15,7 +15,7 @@ int main()
     cout << "Enter two number:" << endl;
     int v1=0, v2=0;
     cin >> v1 >> v2;
-    cout << "The number of " << v1 << " and " << v2
+    cout << "The product of " << v1 << " and " << v2
          << " is "<< v1 * v2<<endl;
 
     return 0;
---
+++ Chapter01/1_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter two number:" << endl;
+    int v1=0, v2=0;
+    cin >> v1 >> v2;
+    cout << "The sum of " ;
+    cout << v1 ;
+    cout << " and " ;
+    cout << v2;
+    cout << " is " ;
+    cout << v1+v2 ;
+    cout << endl;
+
+    return 0;
+}
