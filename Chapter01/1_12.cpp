79692ffbad0386ed740f0bb586b943debb88f5aa
Yiyiya <jingfu1991@gmail.com>
2018-08-24 14:13:04 +0800

    1_4_2

---
+++ Chapter01/1_12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum =0;
+    for (int i =-100; i<=100; ++i)
+        sum +=i;
+    cout << "Sum of -100 to 100 inclusive is "
+         << sum << endl;
+
+    return 0;
+}
