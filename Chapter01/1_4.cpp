701ae660461bc92d11ffdc4d086b92c302b85274
Yiyiya <jingfu1991@gmail.com>
2018-08-24 12:41:45 +0800

    chapter1_4

---
+++ Chapter01/1_4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    cout << "Enter two number:" << endl;
+//    int v1=0, v2=0;
+//    cin >> v1 >> v2;
+//    cout << "The number of " << v1 << " and " << v2
+//         << " is "<< v1+v2<<endl;
+
+//    cout << " Hello World !" << endl;
+
+    cout << "Enter two number:" << endl;
+    int v1=0, v2=0;
+    cin >> v1 >> v2;
+    cout << "The number of " << v1 << " and " << v2
+         << " is "<< v1 * v2<<endl;
+
+    return 0;
+}
