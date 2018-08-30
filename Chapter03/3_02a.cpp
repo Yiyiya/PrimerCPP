4a2328cace4931b14692489c9cfa2c7e15cee10f
Jing <jingfu1991@gmail.com>
2018-08-30 21:36:58 +0800

    Add 3_02

---
+++ Chapter03/3_02a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::string;
+using namespace std;
+int main()
+{
+    string line;
+
+    while ( getline(cin, line) )
+        if (line.empty())
+            cout << line << endl;
+
+    return 0;
+}
