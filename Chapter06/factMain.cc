353a9c8b36f56a7b42cb6a311bb727d1114f8d4c
Jing <jingfu1991@gmail.com>
2018-09-10 15:10:38 +0800

    分离式编译

---
+++ Chapter06/factMain.cc
@@ -0,0 +1,10 @@
+#include "Chapter6.h"
+#include <iostream>
+
+using namespace std;
+int main
+{
+    cout << "5! is " << fact(5) << endl;
+    cout << func() << endl;
+    cout << abs(-9.78) << endl;
+}
